3cfbcb8e53c581613895237c76b63ac185508643
Evgeny Gaidukevich <114091271+Fountainebleu@users.noreply.github.com>
2024-10-09 16:08:04 +0500

    добавил игрока и базовые механики передвижения

---
+++ MolerProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpSpeed;
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+        }
+    }
+}
