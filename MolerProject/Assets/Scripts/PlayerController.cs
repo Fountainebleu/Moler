2dd7e614764a308d690b2c87ebca2c651ecffeb6
Evgeny Gaidukevich <114091271+Fountainebleu@users.noreply.github.com>
2024-10-31 22:46:45 +0500

    подкоп

--- MolerProject/Assets/Scripts/PlayerController.cs
+++ MolerProject/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
@@ -15,7 +16,8 @@ public class PlayerController : MonoBehaviour
     private GameObject playerObject;
     private Rigidbody2D rb2d;
     private Collider2D col2d;
-    
+
+    private bool underGround = false;
     bool isGrounded;
     private void Awake()
     {
@@ -24,20 +26,53 @@ public class PlayerController : MonoBehaviour
         col2d = GetComponent<Collider2D>();
     }
 
-    
+    private void FixedUpdate()
+    {
+        if (underGround)
+        {
+            UnderGroundMove();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            col2d.isTrigger = false;
+            underGround = false;
+            rb2d.gravityScale = 1;
+        }
+    }
+
     private void Update()
     {
         bool isGrounded = Controls.isGrounded(col2d, groundLayer);
-        if (isGrounded)
+        if (!underGround && Input.GetKeyDown(KeyCode.F) && isGrounded)
         {
-            Controls.Move(rb2d, Input.GetAxis("Horizontal"), Input.GetAxis("Debug Horizontal"), speed, Input.GetAxis("SpeedUp") * speedUp);
-        }
+            col2d.isTrigger = true;
+            rb2d.gravityScale = 0;
+            underGround = true;
+            transform.position = new Vector2(transform.position.x, transform.position.y - 1.5f);
 
-        if(Controls.isGrounded(col2d, groundLayer) && Input.GetButtonDown("Jump"))
+        }
+        else
         {
-            Controls.Jump(rb2d, jumpSpeed);
-        }
+            if (isGrounded)
+            {
+                Controls.Move(rb2d, Input.GetAxis("Horizontal"), Input.GetAxis("Debug Horizontal"), speed, Input.GetAxis("SpeedUp") * speedUp);
+            }
 
+            if(Controls.isGrounded(col2d, groundLayer) && Input.GetButtonDown("Jump"))
+            {
+                Controls.Jump(rb2d, jumpSpeed);
+            }
+        }
         Controls.WhereCharLook(playerObject, rb2d);
+    }
+
+    private void UnderGroundMove()
+    {
+        rb2d.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+        
     }
 }
